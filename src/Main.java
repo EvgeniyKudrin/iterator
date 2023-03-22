314570bc30337664f5d5da01b1f3c53d62c168c0
EvgeniyKudrin <Evgeniy9510@yandex.ru>
2023-03-22 21:46:54 +0300

    исправления

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int r : new Randoms(90, 100)) {
             System.out.println("Случайное число: " + r);
+            TimeUnit.SECONDS.sleep(1);
             if (r == 100) {
                 System.out.println("Выпало число 100, давайте на этом закончим");
                 break;
--- src/Randoms.java
+++ src/Randoms.java
@@ -1,28 +1,29 @@
 import java.util.Iterator;
-import java.util.PrimitiveIterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    protected PrimitiveIterator.OfInt randomIterator;
+    protected Random random;
     protected int min;
     protected int max;
 
     public Randoms(int min, int max) {
-        randomIterator = new Random().ints(min, (max + 1)).iterator();
+        this.random = new Random();
         this.min = min;
         this.max = max;
     }
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<>() {
+            boolean isList = true;
+            int nextNum = 0;
             @Override
             public boolean hasNext() {
-                return true;
+                return isList;
             }
 
             @Override
             public Integer next() {
-                return randomIterator.nextInt();
+                return random.nextInt((max - min) + 1) + min;
             }
         };
     }
